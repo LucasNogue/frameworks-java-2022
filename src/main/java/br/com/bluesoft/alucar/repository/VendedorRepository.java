767467f8d01c2dfaa66ecac4b6aa2209df385d20
LucasNogueira <lucas240503@gmail.com>
2022-11-09 16:50:34 -0300

    feat: criando ContaCorrente e o seu endpoint

--- src/main/java/br/com/bluesoft/alucar/repository/VendedorRepository.java
+++ src/main/java/br/com/bluesoft/alucar/repository/VendedorRepository.java
@@ -1,7 +1,11 @@
 package br.com.bluesoft.alucar.repository;
 
+import br.com.bluesoft.alucar.model.Cliente;
 import br.com.bluesoft.alucar.model.Vendedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
+    Optional<Vendedor> findByNome(String nome);
 }
